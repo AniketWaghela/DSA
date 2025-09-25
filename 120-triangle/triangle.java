bedaa795957aa10c0b80ca450bab0bdc4d503dcd
AniketWaghela <141416945+AniketWaghela@users.noreply.github.com>
2025-09-25 12:00:39 +0530

    Time: 10 ms (6.00%) | Memory: 46 MB (7.00%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,15 +1,10 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        
-
+    
         for(int i =triangle.size()-2; i>=0 ; i--){
             for(int j=0; j<=i ;j++){
-                int curr = triangle.get(i).get(j);
-                int left = triangle.get(i+1).get(j);
-                int right = triangle.get(i+1).get(j+1);
-                triangle.get(i).set(j , Math.min( curr+right , curr+left ) );
+                triangle.get(i).set(j , Math.min( triangle.get(i).get(j)+triangle.get(i+1).get(j+1) , triangle.get(i).get(j)+triangle.get(i+1).get(j) ) );
             }
-            // System.out.println();
         }
 
         return triangle.get(0).get(0);
