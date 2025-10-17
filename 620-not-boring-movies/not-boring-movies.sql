868f4237f27ca3d40a1fd56ab84bdc67fc1f66b7
AniketWaghela <141416945+AniketWaghela@users.noreply.github.com>
2025-10-17 15:05:45 +0530

    Time: 247 ms (33.20%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,7 +1,6 @@
 # Write your MySQL query statement below
 
 SELECT *
-FROM Cinema
-WHERE (id%2) = 1
-    AND description != 'boring'
+FROM cinema
+WHERE id%2 = 1 AND description != 'boring'
 ORDER BY rating DESC;
